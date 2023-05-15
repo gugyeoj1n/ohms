cc1ec656d640adad646e4d7e0daf8ed5212c1401
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2023-05-16 00:02:27 +0900

    add stamina, sprint function

--- ohms-source/Assets/Scripts/Player/PlayerMove.cs
+++ ohms-source/Assets/Scripts/Player/PlayerMove.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
+    public float moveSpeed = 1.0f;
     public float rotateSpeed = 180f;
+    public bool sprinting = false;
+    public float stamina = 1f;
+    public float staminaUseSpeed = 0.005f;
+    float maxStamina = 1f;
+    float lerpSpeed = 10f;
 
     private PlayerInput playerInput;
     private Rigidbody playerRigidbody;
     private Animator playerAnim;
+
+    public Slider StaminaBar;
 
     void Start()
     {
@@ -23,6 +31,32 @@ public class PlayerMove : MonoBehaviour
         Rotate();
         Move();
         playerAnim.SetFloat("Move", playerInput.move);
+        if(Input.GetKey(KeyCode.LeftShift) && stamina > 0f)
+        {
+            sprinting = true;
+            moveSpeed = 4f;
+        }
+        else
+        {
+            sprinting = false;
+            moveSpeed = 1f;
+        }
+        Sprint(sprinting);
+        playerAnim.SetBool("Sprint", sprinting);
+    }
+
+    void Sprint(bool s)
+    {
+        if(s)
+        {
+            stamina -= staminaUseSpeed;
+        }
+        else
+        {
+            if(stamina < maxStamina)
+                stamina += staminaUseSpeed * 0.5f;
+        }
+        StaminaBar.value = Mathf.Lerp(StaminaBar.value, stamina, Time.deltaTime * lerpSpeed);
     }
 
     void Move()
