aa6299e42efea24f62f09ed04a1cb21dcd899494
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2023-05-23 15:51:19 +0900

    create lobby buttons

---
+++ ohms-source/Assets/Scripts/Lobby/LobbyManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private byte maxPlayersPerRoom = 2;
+    string gameVersion = "1.0.0";
+
+    void Start()
+    {
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        
+    }
+}
